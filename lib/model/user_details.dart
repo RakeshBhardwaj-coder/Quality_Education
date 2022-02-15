374d20c2672dbce7f4d2f601fd787301927d09b0
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-02-15 23:49:27 +0530

    a

--- lib/model/user_details.dart
+++ lib/model/user_details.dart
@@ -1,1 +1,20 @@
-import 'package:flutter/rendering.dart';
+import 'dart:core';
+
+class UserDetails {
+  String? displayName;
+  String? email;
+  String? photoUrl;
+  UserDetails({this.displayName, this.email, this.photoUrl});
+  UserDetails.fromJson(Map<String, dynamic> json) {
+    displayName = json["display name"];
+    email = json["email"];
+    photoUrl = json["photoUrl"];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['diaplayName'] = this.displayName;
+    data['email'] = this.email;
+    data['photoUrl'] = this.photoUrl;
+    return data;
+  }
+}
